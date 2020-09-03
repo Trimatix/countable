c75fe3cd79bfb9e23cc8556a3b30369f0e8e6956
Ben Silverman <bensilverman10@gmail.com>
2020-09-03 10:20:49 +0100

    Add commented-out channel topic modification

--- commands/set-count.js
+++ commands/set-count.js
@@ -14,13 +14,23 @@ module.exports = {
 			return message.channel.send("I'm sorry, that's not a valid number.");
 		}
 
+		// Parse the argument as an integer and update the client's settings.
 		message.client.settings.set(message.guild.id, parseInt(args[0], 10), "nextCount");
 
+		// Update the channel topic with the new value in nextCount.
+		// message.client.channels
+		// 	.fetch(message.client.settings.get(message.guild.id, "countingChannelID"))
+		// 	.then((countingChannel) =>
+		// 		countingChannel
+		// 			.setTopic(message.client.settings.get(message.guild.id, "nextCount"))
+		// 			.catch((e) => console.error(e)),
+		// 	);
+
 		message.channel.send(
-			`The count has been updated to ${message.client.settings.get(
+			`The count has been updated to \`${message.client.settings.get(
 				message.guild.id,
 				"nextCount",
-			)}`,
+			)}\`.`,
 		);
 	},
 };
